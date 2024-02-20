77820e42172fc65d4ef006c7aed890d9cfaa9662
Narybyun <147260576+Narybyun@users.noreply.github.com>
2024-02-21 00:15:44 +1100

    first commit

---
+++ main.js
@@ -0,0 +1,12 @@
+const API_KEY = 'API_KEY';
+let news = [];
+const getLatestNews = async () =>{
+    const url = new url('http://times-node-env.eba-appvq3ef.ap-northeast-2.elasticbeanstalk.com/top-headlines');
+    const response = await fetch(url);
+    const data = await response.json();
+    news = data.articles;
+    console.log("ddd", news);
+};
+
+getLatestNews();
+
